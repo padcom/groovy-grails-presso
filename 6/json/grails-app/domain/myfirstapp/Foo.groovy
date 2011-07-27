bec2057e40ebc654d0b473b187d095de90de64a1
Maciej Hryniszak <maciej.hryniszak@travelocity.com>
2011-07-27 13:12:43 +0200

    Code cleanup

--- 6/json/grails-app/domain/myfirstapp/Foo.groovy
+++ 6/json/grails-app/domain/myfirstapp/Foo.groovy
@@ -1,12 +1,11 @@
 package myfirstapp
 
 class Foo {
-
-String name
-String someField
+	String name
+	String someField
     
-static constraints = {
-	name(nullable: false, blank: false)
-	someField(nullable: true, blank: true)
+	static constraints = {
+		name       nullable: false, blank: false
+		someField  nullable: true, blank: true
     }
 }
--- 6/validating/grails-app/domain/myfirstapp/Foo.groovy
+++ 6/validating/grails-app/domain/myfirstapp/Foo.groovy
@@ -12,3 +12,17 @@ static constraints = {
 	})
     }
 }
+
+package myfirstapp
+
+class Foo {
+	String name
+	String someField
+    
+	static constraints = {
+		name       nullable: false, blank: false
+		someField  nullable: true, blank: true, validator: {
+	    	it == 'ala' ? true : ["foo.notAla"]
+		}
+    }
+}
